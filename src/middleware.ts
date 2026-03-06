fbb3831f9eea667f8ce13bb24daff28dbad10eef
kairos-global <kairosglobalapp@gmail.com>
2026-03-06 01:17:13 -0700

    goyo

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,30 @@
-export * from "../middleware";
-export { default } from "../middleware";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/community",
+  "/search",
+  "/terms",
+  "/privacy",
+  "/copyright",
+  "/community-guidelines",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isPublicRoute(req)) {
+    return;
+  }
+
+  const session = await auth();
+
+  if (!session.userId) {
+    return session.redirectToSignIn();
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
 
